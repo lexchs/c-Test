c2f25d99afbffcf0c58498f91eb181ba3b318e5e
Alexey <alexcheremisin@mail.ru>
2023-02-22 20:47:59 +0300

    .

---
+++ examples005_Ifelse/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите имя пользователя:");
+string username = Console.Readline();
+
+if(username == "Даша")
+{
+    Console.WriteLine("Ура, это же мой кiт!");
+
+}
+
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
+Console.WriteLine("Hello, World!");
